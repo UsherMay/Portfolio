2001299764c29e0d43dcd0aa6acbbe26651166e5
UsherMay <98615933+UsherMay@users.noreply.github.com>
2025-03-06 19:08:47 +0100

    Update NavBar.jsx
    NavBar at the top of the page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ function NavBar({isMenuOpen, setIsMenuOpen}){
     }, [isMenuOpen]);
 
     return(
-        <nav className="fixed top-8 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+        <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
             <div className=" max-w-5xl mx-auto px-4">
                 <div className="flex justify-between items-center h-16">
                     <a href="#home" className="font-mono text-xl font-bold text-white"> 
