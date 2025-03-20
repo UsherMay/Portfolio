a11585a64e4c832c9b4509b705c1461a7845d54a
UsherMay <98615933+UsherMay@users.noreply.github.com>
2025-03-20 15:17:51 +0100

    Update Resume.jsx

--- src/components/sections/Resume.jsx
+++ src/components/sections/Resume.jsx
@@ -13,9 +13,9 @@ import 'react-pdf/dist/esm/Page/TextLayer.css';
 function Resume(){
 
     const [containerRef, setContainerRef] = useState(null); // <HTMLElement | null>
-    const [containerWidth, setContainerWidth] = useState(0); // <number>
+    const [containerWidth, setContainerWidth] = useState(100); // <number>
 
-    const maxWidth = 800;
+    const maxWidth = 500;
 
     const onResize = useCallback((entries) => {
         const [entry] = entries;
@@ -26,17 +26,22 @@ function Resume(){
       }, []);
     
 
+    //useResizeObserver(containerRef, resizeObserverOptions, onResize);
+
     return(
         <section
             id="resume"
             className="min-h-screen flex items-center justify-center py-20"     
         >
-
-            {/* <h2 className="text-center text-3xl font-bold mb-8 bg-gradient-to-r from-yellow-500 to-orange-600 bg-clip-text text-transparent leading-right">
+            {/* 
+            <h2 className="text-center text-3xl font-bold mb-8 bg-gradient-to-r from-yellow-500 to-orange-600 bg-clip-text text-transparent leading-right">
                 Curriculum vitæ
-            </h2> */}
+            </h2> 
+            */}
 
-            <div className="grid grid-cols-1 md:grid-cols2 gap-6 mt-8">
+            
+            { console.log(containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth,containerWidth)}
+            <div className="grid grid-cols-1 md:grid-cols2 gap-4">
                 <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all" ref={setContainerRef}>
                     <Document
                         file={"/src/assets/CV_Yvon_COCKS.pdf"}
@@ -54,6 +59,7 @@ function Resume(){
                         }}
                     /> */}
                 </div>
+                
             </div>
         </section>
     );
