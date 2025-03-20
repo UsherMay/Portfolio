2717f91d2a93995804ccd3fdaad10b2f9965a099
UsherMay <98615933+UsherMay@users.noreply.github.com>
2025-03-20 18:41:45 +0100

    add packages for react-pdf

--- src/components/sections/Resume.jsx
+++ src/components/sections/Resume.jsx
@@ -20,7 +20,8 @@ const maxWidth = 800;
 
 
 function Resume(){
-    // const [file, setFile] = useState('/src/assets/CV_Yvon_COCKS.pdf');
+    //const [file, setFile] = useState("/src/assets/CV_Yvon_COCKS_merged.pdf");//'/src/assets/CV_Yvon_COCKS.pdf');
+    
     const [numPages, setNumPages] = useState();
     const [containerRef, setContainerRef] = useState(null); // <HTMLElement | null>
     const [containerWidth, setContainerWidth] = useState(); // <number>
@@ -28,7 +29,7 @@ function Resume(){
     const onResize = useCallback((entries) => {
         const [entry] = entries;
     
-        console.log("onResize => entry.contentRect.width : ",entry.contentRect.width);
+        // console.log("onResize => entry.contentRect.width : ",entry.contentRect.width);
         if (entry) {
 
             setContainerWidth(entry.contentRect.width);
@@ -48,31 +49,59 @@ function Resume(){
         >
             
             <h2 className="text-center text-3xl font-bold mb-8 bg-gradient-to-r from-yellow-500 to-orange-600 bg-clip-text text-transparent leading-right">
-                Curriculum vitæ
+                Curriculum Vitæ
             </h2> 
             
-            { console.log(containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth, containerWidth)/*className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all"*/ }
+            {/* { console.log(containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth, containerWidth) } */}
+            {/* className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all" */}
              
-                <div className="w-8/12 max-w-[calc(100%_-_2rem)] m-0.5 p-6 rounded border border-white/10 hover:-translate-y-1 transition-transform" ref={setContainerRef}> 
+                <div className="w-6/12 max-w-[calc(100%_-_2rem)] m-0.5 p-6 rounded-xl border border-white/10 hover:-translate-y-1 transition-transform" ref={setContainerRef}> 
                 {/* <div className='p-6 rounded-xl border border-white/10 hover:-translate-y-1 transition-transform' ref={setContainerRef}> */}
                         <Document
-                            file={"/src/assets/CV_Yvon_COCKS_merged.pdf"}
-                            // style={{ flexDirection: "cols" }}
+                            file={'/src/assets/CV_Yvon_COCKS.pdf'}
                             onLoadError={console.error}
                             onLoadSuccess={onDocumentLoadSuccess}
                             options={options}
                             >
-                                {Array.from(new Array(numPages), (_el, index) => (
+                                {/* <div className="max-w-full flex mt-4 self-center">
+                                    <button disabled={(pageNumber || 0) <= 1} onClick={previousPage} type="button">
+                                    Previous
+                                    </button>
+                                    <span>{`Page ${pageNumber || (numPages ? 1 : '--')} of ${numPages || '--'}`}</span>
+                                    <button
+                                    disabled={(pageNumber || 0) >= (numPages || 0)}
+                                    onClick={nextPage}
+                                    type="button"
+                                    >
+                                    Next
+                                    </button>
+                                </div> */}
+                                {/* {Array.from(new Array(numPages), (_el, index) => (
                                               <Page
                                                 key={`page_${index + 1}`}
                                                 pageNumber={index + 1}
                                                 width={containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth}
                                               />
-                                            ))}
-                                {/* <Page 
+                                            ))} */}
+                                <Page 
                                     pageIndex={0}
                                     width={containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth}
-                                /> */}
+                                />
+                        </Document>
+                    
+                </div>
+                
+                <div className="w-6/12 max-w-[calc(100%_-_2rem)] m-0.5 p-6 rounded-xl border border-white/10 hover:-translate-y-1 transition-transform" ref={setContainerRef}> 
+                        <Document
+                            file={'/src/assets/CV_Yvon_COCKS_simple.pdf'}
+                            onLoadError={console.error}
+                            onLoadSuccess={onDocumentLoadSuccess}
+                            options={options}
+                            >
+                                <Page 
+                                    pageIndex={0}
+                                    width={containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth}
+                                />
                         </Document>
                     
                 </div>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,40 @@
-import { defineConfig } from 'vite'
+import { defineConfig, normalizePath } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import path from 'node:path';
+import { createRequire } from 'node:module';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+const require = createRequire(import.meta.url);
+const pdfjsDistPath = path.dirname(require.resolve('pdfjs-dist/package.json'));
+const cMapsDir = normalizePath(path.join(pdfjsDistPath, 'cmaps'));
+const standardFontsDir = normalizePath(
+  path.join(path.dirname(require.resolve('pdfjs-dist/package.json')), 'standard_fonts')
+);
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    viteStaticCopy({
+           targets: [
+             {
+                src: cMapsDir,
+                dest: '',
+             },
+             {
+                src: standardFontsDir,
+                dest: '',
+              },
+           ],
+         })
+  ],
 })
 
 // in terminal => npm install tailwindcss @tailwindcss/vite
+// https://github.com/wojtekmaj/react-pdf?tab=readme-ov-file#support-for-non-latin-characters
+
+
+
