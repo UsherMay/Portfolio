0ce4f4a5d486e9d415fbf0dc6898ccde482316ec
UsherMay <98615933+UsherMay@users.noreply.github.com>
2025-06-26 13:25:56 +0200

    fixes + add packages

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -9,12 +9,12 @@ function Contact(){
             <div className="max-w-3xl mx-auto px-4">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6 border rounded-2xl border-white/10 hover:-translate-y-1 transition-all">
 
-                <div className="p-4 flex flex-col items-center gap-5">
-                    <h2 className="text-center text-4xl tracking-widest font-bold bg-gradient-to-r from-yellow-500 to-orange-600 bg-clip-text text-transparent leading-right">
-                    Contacts
-                    </h2>
-                    <p className="p-4 text-center text-xl tracking-wide font-bold">N'hésitez pas à me contacter</p>
-                </div>    
+                    <div className="p-4 flex flex-col items-center gap-5">
+                        <h2 className="text-center text-4xl tracking-widest font-bold bg-gradient-to-r from-yellow-500 to-orange-600 bg-clip-text text-transparent leading-right">
+                        Contacts
+                        </h2>
+                        <p className="p-4 text-center text-xl tracking-wide font-bold">N'hésitez pas à me contacter</p>
+                    </div>    
                 
                     <div className="p-4">
                         <ul className="flex flex-col items-center gap-5">
